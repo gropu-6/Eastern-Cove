e7dac4aa69cbeb370cdd207e2862a871e018c90d
YON <dlncg4602@gmail.com>
2021-02-23 15:37:55 +0000

    added game sprites not animated yet
    pls look at the gdd and figure some brief points that you guys can work on

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Mouse Position
+    public Vector2 mousePosition = new Vector2(0.0f, 0.0f);
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // If the player presses the left button
+        {
+            // Do something here.
+            Debug.Log(mousePosition[0].ToString() + " " + mousePosition[1].ToString());
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Translating the worlds position of the mouse to the screens position.
+    }
+}
