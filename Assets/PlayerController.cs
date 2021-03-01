975e9eb7050c5cffcb4047f829169ebe830d2198
YON <dlncg4602@gmail.com>
2021-03-01 13:47:42 +0000

    player movement and some animations
    did some stuff to the playercontroller code

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Mouse Position
-    public Vector2 mousePosition = new Vector2(0.0f, 0.0f);
+
+    public Vector2 mousePosition; // Used to transform Input.mousePosition into a world position.
+    public Vector2 targetPosition; // What position we are moving the player towards, calculated by the x and y of mousePosition and the current set pivot point.
+    public Transform pivot; // Determines which part of the body meets the clicked position.
+    private Animator animator;
+    private SpriteRenderer sprite;
+    bool walk = false; // A check to see if the player is walking to perform animations.
+
+    void Start(){
+
+        animator = transform.GetChild(0).GetComponent<Animator>();
+        sprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+
+    }
 
     private void Update()
     {
+        animator.SetBool("walking", walk);
+
         if (Input.GetMouseButtonDown(0)) // If the player presses the left button
         {
-            // Do something here.
-            Debug.Log(mousePosition[0].ToString() + " " + mousePosition[1].ToString());
+            targetPosition = new Vector2(mousePosition.x - pivot.localPosition.x, mousePosition.y - pivot.localPosition.y);
 
+            if(transform.position.x < targetPosition.x){
+                sprite.flipX = true;
+            } else
+            {
+                sprite.flipX = false;
+            }
+            walk = true;
+        }
+
+        print(Vector2.Distance(new Vector2(pivot.position.x, pivot.position.y), targetPosition));
+
+        if(Vector2.Distance(new Vector2(pivot.position.x, pivot.position.y), targetPosition) > 1.4f && Vector2.Distance(new Vector2(pivot.position.x, pivot.position.y), targetPosition) < 1.41f){
+            walk = false;
+        }
+
+        if(walk){
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, 5 * Time.deltaTime);
         }
     }
 
