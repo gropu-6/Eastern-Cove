7fa81455b4862edbab10efbdc883883c5f8bf05f
YON <dlncg4602@gmail.com>
2021-03-02 22:26:58 +0000

    player movement
    player now stops accurately

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,8 +8,14 @@ public class PlayerController : MonoBehaviour
     public Vector2 mousePosition; // Used to transform Input.mousePosition into a world position.
     public Vector2 targetPosition; // What position we are moving the player towards, calculated by the x and y of mousePosition and the current set pivot point.
     public Transform pivot; // Determines which part of the body meets the clicked position.
+    public BoxCollider2D stop;
+
+    int layerToCheck;
+    public Collider2D[] hits = new Collider2D[3];
+
     private Animator animator;
     private SpriteRenderer sprite;
+    
     bool walk = false; // A check to see if the player is walking to perform animations.
 
     void Start(){
@@ -21,29 +27,41 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        
+
         animator.SetBool("walking", walk);
 
         if (Input.GetMouseButtonDown(0)) // If the player presses the left button
         {
-            targetPosition = new Vector2(mousePosition.x - pivot.localPosition.x, mousePosition.y - pivot.localPosition.y);
-
-            if(transform.position.x < targetPosition.x){
-                sprite.flipX = true;
-            } else
-            {
-                sprite.flipX = false;
-            }
-            walk = true;
-        }
-
-        print(Vector2.Distance(new Vector2(pivot.position.x, pivot.position.y), targetPosition));
-
-        if(Vector2.Distance(new Vector2(pivot.position.x, pivot.position.y), targetPosition) > 1.4f && Vector2.Distance(new Vector2(pivot.position.x, pivot.position.y), targetPosition) < 1.41f){
-            walk = false;
+            RayCastClick();
         }
 
         if(walk){
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, 5 * Time.deltaTime);
+
+            hits = Physics2D.OverlapPointAll(pivot.position, 5);
+
+            for(int i = 0; i < hits.Length; ++i){
+                if(hits[i].gameObject.tag == "Mouse"){
+                walk = false;
+                }
+            }
+        
+        }
+
+        
+    }
+
+    private void RayCastClick(){
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Camera.main.transform.forward * 1000.0f);
+
+        if(hit.collider != null){
+            
+            if(transform.position.x < hit.point.x) sprite.flipX = true; else sprite.flipX = false;
+            targetPosition = new Vector2(hit.point.x - pivot.localPosition.x, hit.point.y - pivot.localPosition.y);
+            stop.transform.position = hit.point;
+            walk = true;
         }
     }
 
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
     public List<GameObject> screens = new List<GameObject>(); // Switch between different screens with ease.
+    public Animator fade;
 
     private void Awake()
     {
@@ -16,7 +17,26 @@ public class MenuManager : MonoBehaviour
     public static void initiateGame()
     {
         // We're using the UnityEngine.UI library to load a scene.
-        SceneManager.LoadScene(1); // This will load the main game scene which has an index of 1. It can be seen in File > Build Settings.
+            SceneManager.LoadScene(1); // This will load the main game scene which has an index of 1. It can be seen in File > Build Settings.
+    }
+
+    public void startCoroutineIn()
+    {
+        StartCoroutine(fadeIn());
+    }
+
+    private IEnumerator fadeIn()
+    {
+        fade.SetBool("fadeIn", true);
+
+        yield return new WaitForSeconds(1.5f);
+
+        initiateGame();
+    }
+
+    private void fadeOut()
+    {
+
     }
 
     public void changeScreen(int index)
@@ -30,5 +50,6 @@ public class MenuManager : MonoBehaviour
     public static void quitGame()
     {
         Application.Quit();
+        GUIUtility.ExitGUI();
     }
 }
