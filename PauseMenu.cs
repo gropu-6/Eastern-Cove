60e66fe6090b15250f899f27091ba0d940d7f981
PapsOnGo <78440451+PapsOnGo@users.noreply.github.com>
2021-04-30 00:20:06 +0100

    Add files via upload

---
+++ PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetkeyDown(KeyCode.Escape))
+        {
+            if (GameIsPausedIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+    }
+}
